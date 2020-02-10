2d7b180c2dceacc3cbf4db1ca1ca6450f6147439
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-10 22:05:41 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,6 +27,7 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owner = @pet.owner
+    erb :'/pets/edit'
   end
 
   patch '/pets/:id' do
