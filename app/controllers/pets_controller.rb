9058ba33b996ded87520a3451872cbc917158f2c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-10 21:41:29 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,10 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      owner = Owner.create(params[:owner][:name])
+      owner.pets << @pet
+    end
     binding.pry
     redirect to "pets/#{@pet.id}"
   end
